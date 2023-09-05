35ac1e2264f9f7aba80b848689a4cc374b363a9e
simjust175 <bunim175@gmail.com>
2023-09-05 13:35:37 +0300

    some more touch-ups

--- src/coinCards/coinCards.js
+++ src/coinCards/coinCards.js
@@ -1,161 +1,144 @@
 // DOM selector utility function
-const DOMselector = (elementName) => {
-  return document.querySelector(elementName);
-};
+const DOMselector = (element) => document.querySelector(element);
 
-// function to add commas to numbers using .toLocaleString() function
-const addCommatoNumber = (number) => number.toLocaleString();
+// Function to add commas to numbers
+const formatNumber = (number) => number.toLocaleString();
 
-
-// Coin card template
+// Template to generate a coin card
 const coinCard = (coin) => {
-  const { id, name, image: { large, small }, } = coin;
+  const { id, name, image: { large, small } } = coin;
   return `
-      <div class="coinCard" data-id="${id}">
-        <button type="button" class="addCoinButton" data-id="${id}" data-name="${name}" data-img="${small}"><h3>+</h3></button>
-        <div class="coinName">
-          <h3>${name}</h3>
-        </div>
-        <div>
-          <img class="cryptoIcons" src="${large}" alt="${id}">
-        </div>
-        <div class="coinPrice" id="coinPrice${id}"></div>
-        <div class="pastDayChange" id="pastDayChange${id}"></div>
-        <div class="overlay">
-          <button type="button" class="getRateButton">Get rate</button>
-        </div>
+    <div class="coinCard" data-id="${id}">
+      <button type="button" class="addCoinButton" data-id="${id}" data-name="${name}" data-img="${small}">
+        <h3>+</h3>
+     </button>
+      <i class='bx bxs-heart' id="favorite${name}"></i>
+      <div class="coinName">
+        <h3>${name}</h3>
       </div>
-    `;
-};
+     <img class="cryptoIcons" src="${large}" alt="${id}">
+      <div class="coinPrice" id="coinPrice${id}"></div>
+      <div class="pastDayChange" id="pastDayChange${id}"></div>
+      <div class="overlay">
+         <button type="button" class="getRateButton">Get rate</button>
+      </div>
+   </div>
+`};
 
-// Event listener function for getRate buttons
-const addEventListenersToButtons = () => {
-  const getRateButtons = document.querySelectorAll(".getRateButton");
+// Function to attach event listeners to get rate buttons
+const attachRateButtonListeners = () => {
+  const getRateButtons = document.querySelectorAll(".getRateButton")
   getRateButtons.forEach((button) => {
-    button.addEventListener("click", priceEventHandler);
+    button.addEventListener("click", handleRateButtonClick);
   });
 };
 
-// Event listener function for addCoin buttons
-const addEventListenersToAddCoinButtons = () => {
+// Function to attach event listeners to add coin buttons
+const attachAddCoinButtonListeners = () => {
   const addCoinButtons = document.querySelectorAll(".addCoinButton");
   addCoinButtons.forEach((button) => {
-    button.addEventListener("click", addCoinEventHandler);
+    button.addEventListener("click", handleAddCoinButtonClick);
   });
 };
 
-// Render coin cards
-const renderCard = (coins) => {
-  const mainCoinDiv = DOMselector(".coins");
-  mainCoinDiv.innerHTML = "";
+// Function to render coin cards
+const renderCards = (coins) => {
+  const mainDiv = DOMselector(".coins");
+  mainDiv.innerHTML = ""
+  //mainDiv.innerHTML = coins.map(coinCard);
   coins.forEach((coin) => {
-    mainCoinDiv.innerHTML += coinCard(coin);
+    mainDiv.innerHTML += coinCard(coin);
   });
 };
 
-// Search bar element
-const searchBar = DOMselector("#searchBar");
+// Function to filter coin results based on search
+const filterResults = (data, query) => data.filter((coin) =>
+  coin.name.toLowerCase().includes(query.toLowerCase())
+);
 
-// Filter coin results based on search
-const filterResults = (data) => {
-  const searchResults = searchBar.value.toLowerCase();
-  return data.filter((coin) => {
-    return coin.name.toLowerCase().includes(searchResults);
-  });
-};
-
-// Load coins and set up event listeners
-const loadCoin = async () => {
+// Async function to load coins and setup event listeners
+const loadCoins = async () => {
   const res = await fetch("https://api.coingecko.com/api/v3/coins/");
-  const data = await res.json();
-  renderCard(data);
-  addEventListenersToButtons();
-  addEventListenersToAddCoinButtons();
+  const coins = await res.json();
 
-  searchBar.addEventListener("input", () => {
-    const filteredCoins = filterResults(data);
-    renderCard(filteredCoins);
-    addEventListenersToButtons();
-    addEventListenersToAddCoinButtons();
+  renderCards(coins);
+  attachRateButtonListeners();
+  attachAddCoinButtonListeners();
+
+  DOMselector("#searchBar").addEventListener("input", (e) => {
+    const filteredCoins = filterResults(coins, e.target.value);
+    renderCards(filteredCoins);
+    attachRateButtonListeners();
+    attachAddCoinButtonListeners();
   });
 };
 
-// Event handler for price button
-const priceEventHandler = (event) => {
-  const target = event.target.parentElement;
+// Event handler for get rate button click
+const handleRateButtonClick = (e) => {
+  
+  // remove the overlay
+  const target = e.target.parentElement;
   target.classList.add("hidden");
-  const currentCard = target.parentElement;
-  const currentCoin = currentCard.getAttribute("data-id");
-  getRate(currentCoin, 'main');
+
+  const card = e.target.closest(".coinCard");
+  const coinId = card.getAttribute("data-id");
+  getRate(coinId, true);
 };
 
-// Fetch coin rate
-const getRate = async (currentCoin, main) => {
+// Async function to fetch coin rate
+const getRate = async (coinId, main) => {
   try {
-    const res = await fetch(
-      `https://api.coingecko.com/api/v3/coins/${currentCoin}`
-    );
+    const res = await fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`);
     const data = await res.json();
-    const selectedCurrency = currency; // Get the selected currency
+    selectedCurrency = currency;
     main ? renderRate(data, selectedCurrency) : data;
   } catch (error) {
-    throw new Error("An error occurred: " + error);
+    console.error("An error occurred:", error);
   }
 };
 
-// Select currency
-const usd = DOMselector("#usd");
-const ils = DOMselector("#ils");
+// Function to render coin rate
+const renderRate = ({ id, market_data: { current_price, price_change_24h_in_currency } }, currency) => {
+  const priceDisplay = DOMselector(`#coinPrice${id}`);
+  const changeDisplay = DOMselector(`#pastDayChange${id}`);
+  
+  const currencySymbols = { usd: "$", ils: "₪" };
+  const marketStatus = price_change_24h_in_currency > 0 ? "up" : "down";
+  const currencySymbol = currencySymbols[currency];
+
+  priceDisplay.innerHTML = `${currencySymbol}${formatNumber(current_price[currency])}`;
+  priceDisplay.classList.add(marketStatus);
+
+  changeDisplay.innerHTML = `
+    <i class='bx bxs-${marketStatus}-arrow ${marketStatus}'></i>
+    ${currencySymbol}${formatNumber(price_change_24h_in_currency[currency])}
+  `;
+};
+
 
 // gets the selected currency from the buttons
-let currency = "usd"
+let currency = "usd";
 const currencySelector = (e) => {
   currency = e.target.id;
-  console.log(`is it true? ${currency}`);
   const clickedCurrency = currency ? currency : "usd";
   usd.classList.toggle("selectedCurrency", clickedCurrency === "usd");
   ils.classList.toggle("selectedCurrency", clickedCurrency === "ils");
   return currency;
 };
-
-
 usd.addEventListener("click", currencySelector);
 ils.addEventListener("click", currencySelector);
 
-
-
-// Render coin rate
-const renderRate = (data, selectedCurrency) => {
-  const { id, market_data: { current_price, price_change_24h_in_currency } } = data;
-
-  const priceDisplay = DOMselector(`#coinPrice${id}`);
-  const pastDayChange = DOMselector(`#pastDayChange${id}`);
-
-  const currencyDisplay = {
-    usd: "$",
-    ils: "₪",
-  };
-
-  const marketStatus = price_change_24h_in_currency > 0 ? "up" : "down";
-
-  priceDisplay.innerHTML = `${currencyDisplay[selectedCurrency]}${addCommatoNumber(current_price[selectedCurrency])}`;
-
-  priceDisplay.classList.add(`${marketStatus}`);
-
-  pastDayChange.innerHTML = `<i class='bx bxs-${marketStatus}-arrow ${marketStatus}'></i>
-   ${currencyDisplay[selectedCurrency]}${addCommatoNumber(price_change_24h_in_currency[selectedCurrency])}`;
-};
-
-// Call the loadCoin function to initiate loading of coins
-loadCoin();
+// Call the loadCoins function to initiate loading of coins
+loadCoins();
 
 // add coin to protofolio
 
 const protifolio = [];
 const sortedProtifolio = protifolio.sort();
+const addedMessage = DOMselector(".addToCoinStatus");
 
 // 'regular' function in-order to use 'this'
-function addCoinEventHandler(e) {
+function handleAddCoinButtonClick(e) {
   const target = e.target;
   const eventOnChild = target.localName === 'h3';
   const button = eventOnChild ? target.parentNode : target;
@@ -169,6 +152,9 @@ function addCoinEventHandler(e) {
     });
 
     button.classList.toggle('coinAdded');
+    addedMessage.classList.toggle('hidden');
+    const favorite = DOMselector(`#favorite${coinName}`);
+    favorite.classList.add("favorite")
     button.innerHTML = '-';
   }
 }
@@ -196,6 +182,7 @@ const showProtifolio = async (protifolio) => {
   protifolioDisplay.classList.toggle("hidden");
   protifolioDisplay.innerHTML = "";
   protifolioDisplay.innerHTML = `<div class="exitProtifolio">X</div>`;
+  addEventToExit()
   for (const item of protifolio) {
     const protifolioItem = await protifolioItems(item);
     protifolioDisplay.innerHTML += protifolioItem;
@@ -205,17 +192,20 @@ const showProtifolio = async (protifolio) => {
 };
 
 const removeCoin = (e) => {
-    console.log(e.target);
+    console.log(e.target.coin);
+    
 }
 
 protifolioButton.addEventListener("click", function () { showProtifolio(sortedProtifolio) })
 
 const addEventToRemoveCoin = () => {
-  const removeFromProtifolio = DOMselector(".removeFromProtifolio");
-  removeFromProtifolio.addEventListener("click", removeCoin);
+  const removeFromProtifolio = document.querySelectorAll(".removeFromProtifolio");
+  removeFromProtifolio.forEach( (button) => button.addEventListener("click", removeCoin))
+ ;
 }
 
 const addEventToExit = () => {
   const exitProtifolio = DOMselector(".exitProtifolio");
+  console.log("i am still here!");
   exitProtifolio.addEventListener("click", () => protifolioDisplay.classList.add("hidden"))
 }
